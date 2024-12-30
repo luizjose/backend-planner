92aebad7d94e711de02801d3861dbf99e0ac72a1
luizjose <luiz.jose0597@gmail.com>
2024-12-30 16:25:46 -0300

    teste

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,14 @@ import express from "express";
 //const cors = require("cors");
 import authRoutes from "./routes/authRoutes";
 import protectedRoutes from "./routes/protectedRoutes";
+import { router } from "./routes/routes";
 
 const app = express();
 
 const port = 3000;
 //app.use(cors());
 app.use(express.json());
-app.use("/", (req, res) => {
-  res.send("Welcome to the API");
-});
+app.use(router);
 
 app.use("/api/auth", authRoutes);
 
